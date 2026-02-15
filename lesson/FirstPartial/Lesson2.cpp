e1c8e33b1b1d34760524eb6685fe0d343be0e360
Muhammet Eren <115183696+Merenylmz@users.noreply.github.com>
2026-02-15 22:24:57 +0300

    Second Commit

---
+++ lesson/FirstPartial/Lesson2.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+    string name = "Eren";
+    int old = 18;
+    float noteAverage = 3.18f;
+    double pi = 3.14159265359;
+    bool didPersonPassClass = true;
+    char letterNote = 65; // 'A'
+
+    cout << "Name: " << name <<endl << "My Old: " << old <<endl << "Note Average: " << noteAverage <<endl << "PI: " << pi << endl << "Sinif geçildi mi? " << didPersonPassClass<< endl;
+    cout << "Letter Note: " << letterNote;
+    return 0;
+}
---
+++ lesson/FirstPartial/Lesson3.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+    int number1 = 65;
+    int number2 = 16;
+    float avg = (float) (number1 + number2) / 2;
+    float avg2 = (number1 + number2) / 2;
+    cout << avg << endl;
+    cout << avg2 << endl;
+    return 0;
+}
