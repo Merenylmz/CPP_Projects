7d9079dbb114c55fb1876f67123a56c05683f43a
Muhammet Eren <115183696+Merenylmz@users.noreply.github.com>
2026-02-15 21:33:50 +0300

    First Commit

---
+++ lesson/FirstPartial/Lesson1.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+ 
+    string name = "Eren";
+    int old = 18;
+
+    cout << "ismim "+name <<endl<< "Yasim: " << old; 
+
+    return 0;
+}
